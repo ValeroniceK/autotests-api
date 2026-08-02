03c6bf6d71adb4d9c6826fe863e8789ca8c0ca88
ValeroniceK <v.kosmach11@gmail.com>
2026-08-02 15:00:29 +0300

    Знакомство с Faker

--- clients/authentication/authentication_schema.py
+++ clients/authentication/authentication_schema.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, Field
+from tools.fakers import fake
 
 
 
@@ -15,8 +16,8 @@ class LoginRequestSchema(BaseModel):
     """
     Описание структуры запроса на аутентификацию.
     """
-    email: str
-    password: str
+    email: str = Field(default_factory=fake.email)
+    password: str = Field(default_factory=fake.password)
 
 
 class LoginResponseSchema(BaseModel):
@@ -30,4 +31,4 @@ class RefreshRequestSchema(BaseModel):
     """
     Описание структуры запроса для обновления токена.
     """
-    refresh_token: str = Field(alias="refreshToken")
+    refresh_token: str = Field(alias="refreshToken", default_factory=fake.sentence)
